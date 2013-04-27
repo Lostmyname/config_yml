b9c8781236438967ac78871f71bb95170e75d671
Vitor Kiyoshi Arimitsu <to@vitork.com>
2013-04-27 01:33:32 -0300

    Adding library file and specs

---
+++ lib/configuration.rb
@@ -0,0 +1,36 @@
+module Configuration
+  # Returns a hash with configuration for the given class method
+  # called. The configuration is loaded from the file config/+method_called+.yml.
+  # If the hash contains a key in the first level with the same name of environment,
+  # the value of this key is returned. Returns nil if file is not found.
+  #
+  # For example:
+  #   ## config/database.yml
+  #   # development:
+  #   #   host: "localhost"
+  #   #   username: "root"
+  #
+  #   ENV["RACK_ENV"] # => "development"
+  #   Configuration.database # => { :host => "localhost", :username => "tmp/mysql.sock" }
+  #
+  class << self
+    attr_accessor :hash, :files
+
+    def method_missing(method, *args)
+      @hash  ||= {}
+      @files ||= Dir.glob("config/*.yml")
+
+      if file = @files.select { |f| f =~ /#{method.to_s}/ }[0]
+        @hash[method.to_s] ||= load_yml(file)
+      end
+    end
+
+    private
+
+    def load_yml(file)
+      config = YAML.load_file(file)
+      config.is_a?(Hash) && config.has_key?(ENV["RACK_ENV"].to_sym) ?
+        config[ENV["RACK_ENV"].to_sym] : config
+    end
+  end
+end
---
+++ spec/lib/configuration_spec.rb
@@ -0,0 +1,68 @@
+require "spec_helper"
+
+describe Configuration do
+  let(:files) { ["config/file_a.yml", "config/file_b.yml"] }
+
+  before do
+    Configuration.hash  = nil
+    Configuration.files = nil
+    Dir.stub(:glob).and_return(files)
+    YAML.stub(:load_file).and_return(file_a, file_b)
+  end
+
+  describe "loading config files" do
+    let(:file_a) { double }
+    let(:file_b) { double }
+
+    it "loads all config files" do
+      Configuration.file_a
+      Configuration.files.should be_eql files
+    end
+  end
+
+  describe "acessing a key from configuration files" do
+    context "when a file has a flat hash" do
+      let(:file_a) { { key_a: "value_a" } }
+      let(:file_b) { { key_b: "value_b" } }
+
+      it "responds to keys split in different files" do
+        Configuration.file_a[:key_a].should be_eql "value_a"
+        Configuration.file_b[:key_b].should be_eql "value_b"
+      end
+    end
+
+    context "when a file has a nested hash" do
+      let(:file_a) { { nested: { key: "value" } } }
+      let(:file_b) { double }
+
+      it "responds to nested key values" do
+        Configuration.file_a[:nested][:key].should be_eql "value"
+      end
+    end
+
+    context "when a file first level keys is environment names" do
+      let(:file_b) { double }
+      let(:file_a) do
+        { test: { key: "value_test" },
+          development: { key: "value_dev" } }
+      end
+
+      it "returns hash of environment key" do
+        Configuration.file_a[:key].should be_eql "value_test"
+      end
+
+      it "doesn't load hash of other environments" do
+        Configuration.file_a[:key].should_not be_eql "value_dev"
+      end
+    end
+
+    context "when configuration file doesn't exist" do
+      let(:file_a) { double }
+      let(:file_b) { double }
+
+      it "returns nil" do
+        Configuration.foo.should be_nil
+      end
+    end
+  end
+end
