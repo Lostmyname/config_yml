5c9d04fb1ccfbe087b6bbe4a129cf9bc677b669e
Vitor Kiyoshi Arimitsu <to@vitork.com>
2013-05-10 04:22:44 -0300

    Moving module documentation

--- lib/configuration/syntax/conf.rb
+++ lib/configuration/syntax/conf.rb
@@ -1,15 +1,15 @@
 module Configuration
-  # Provides a shorthand for access config files
-  #
-  # For example:
-  #
-  #   # The following code
-  #   Conf.foo
-  #
-  #   # is the same as:
-  #   Configuration.foo
-  #
   module Syntax
+    # Provides a shorthand for access config files
+    #
+    # For example:
+    #
+    #   # The following code
+    #   Conf.foo
+    #
+    #   # is the same as:
+    #   Configuration.foo
+    #
     module Conf
       def self.method_missing(method, *args)
         Configuration.method_missing(method, args)
