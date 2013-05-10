3b8b28100621ba01232b1aca9ae2cbf68de46daf
Vitor Kiyoshi Arimitsu <to@vitork.com>
2013-05-10 03:53:14 -0300

    Updating spec syntax

--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -4,7 +4,7 @@ describe Configuration do
   let(:files) { ["config/file_a.yml", "config/file_b.yml"] }
 
   before do
-    Configuration.instance_variable_set(:@hash, nil)
+    subject.instance_variable_set(:@hash, nil)
     Dir.stub(:glob).and_return(files)
     YAML.stub(:load_file).and_return(file_a, file_b)
   end
@@ -14,7 +14,7 @@ describe Configuration do
     let(:file_b) { double }
 
     it "loads all config files" do
-      Configuration.files.should be_eql files
+      subject.files.should be_eql files
     end
   end
 
@@ -24,14 +24,14 @@ describe Configuration do
     let(:rack_env) { "rack_test" }
     let(:rails_env) { "rails_test" }
 
-    before { Configuration.instance_variable_set(:@env, nil) }
+    before { subject.instance_variable_set(:@env, nil) }
 
     context "when Rack application" do
       before { ENV["RACK_ENV"] = rack_env }
       after { ENV["RACK_ENV"] = nil }
 
       it "returns the environment as symbol" do
-        Configuration.env.should be_eql rack_env.to_sym
+        subject.env.should be_eql rack_env.to_sym
       end
     end
 
@@ -44,13 +44,13 @@ describe Configuration do
       after { Object.send(:remove_const, :Rails) }
 
       it "returns the environment as symbol" do
-        Configuration.env.should be_eql rails_env.to_sym
+        subject.env.should be_eql rails_env.to_sym
       end
     end
 
     context "when environment isn't defined" do
       it "returns nil" do
-        Configuration.env.should be_nil
+        subject.env.should be_nil
       end
     end
   end
@@ -61,8 +61,8 @@ describe Configuration do
       let(:file_b) { { key_b: "value_b" } }
 
       it "responds to keys split in different files" do
-        Configuration.file_a[:key_a].should be_eql "value_a"
-        Configuration.file_b[:key_b].should be_eql "value_b"
+        subject.file_a[:key_a].should be_eql "value_a"
+        subject.file_b[:key_b].should be_eql "value_b"
       end
     end
 
@@ -71,7 +71,7 @@ describe Configuration do
       let(:file_b) { double }
 
       it "responds to nested key values" do
-        Configuration.file_a[:nested][:key].should be_eql "value"
+        subject.file_a[:nested][:key].should be_eql "value"
       end
     end
 
@@ -86,11 +86,11 @@ describe Configuration do
       after { ENV["RACK_ENV"] = nil }
 
       it "returns hash of environment key" do
-        Configuration.file_a[:key].should be_eql "value_test"
+        subject.file_a[:key].should be_eql "value_test"
       end
 
       it "doesn't load hash of other environments" do
-        Configuration.file_a[:key].should_not be_eql "value_dev"
+        subject.file_a[:key].should_not be_eql "value_dev"
       end
     end
 
@@ -99,7 +99,7 @@ describe Configuration do
       let(:file_b) { double }
 
       it "returns nil" do
-        Configuration.foo.should be_nil
+        subject.foo.should be_nil
       end
     end
   end
